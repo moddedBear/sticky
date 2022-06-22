2bfcd4c4d5ad7a25dde13ff4441d96872787fdd9
Jeremy Payne <jeremypayne26@gmail.com>
2022-06-21 21:03:27 -0600

    Add remove command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
+use std::io::BufReader;
 
 use clap::{Parser, Subcommand};
 use directories_next::ProjectDirs;
@@ -27,10 +28,28 @@ enum Commands {
         note: String,
     },
     /// remove a sticky note
-    Done {
+    Remove {
         /// the index of the note to remove
         #[clap(value_parser)]
         index: u16,
+    },
+    /// mark a sticky note as done
+    Done {
+        /// the index of the note to mark as done
+        #[clap(value_parser)]
+        index: u16,
+    },
+    /// mark a sticky note as undone
+    Undone {
+        /// the index of the note to mark as undone
+        #[clap(value_parser)]
+        index: u16,
+    },
+    /// clear sticky notes marked as done
+    Clear {
+        /// clear all notes regardless of completion status
+        #[clap(short, long, action)]
+        all: bool,
     },
 }
 
@@ -70,9 +89,45 @@ fn append_note(note: &String) {
     }
 }
 
+fn remove_notes(indicies: &Vec<u16>) -> bool {
+    let proj_dirs = get_proj_dirs!();
+    let data_dir = proj_dirs.data_dir();
+    let notes_file = data_dir.join(NOTES_FILENAME);
+    let mut file = &OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(notes_file)
+        .unwrap();
+    let buf = BufReader::new(file);
+    let mut lines: Vec<String> = buf.lines().map(|x| x.unwrap()).collect();
+    for index in indicies {
+        if index >= &(lines.len() as u16) {
+            println!("Note index out of bounds!");
+            return false;
+        }
+        lines.remove(*index as usize);
+    }
+    file.set_len(0).unwrap(); // clear file
+    write!(file, "{}", lines.join("\n")).unwrap();
+    return true;
+}
+
 fn command_add(note: &String) {
     append_note(note);
     println!("Note added!");
+}
+
+fn command_remove(index: &u16) {
+    if *index == 0 {
+        println!("Note index out of bounds!");
+        return;
+    }
+    let adjusted_index = *index - 1;
+    if remove_notes(&vec![adjusted_index]) {
+        println!("Note removed!");
+        return;
+    };
+    println!("Error removing note!");
 }
 
 fn main() {
@@ -86,7 +141,7 @@ fn main() {
 
     match &cli.command {
         Some(Commands::Add { note }) => command_add(note),
-        Some(Commands::Done { index }) => println!("Removing note: {}", index),
+        Some(Commands::Remove { index }) => command_remove(index),
         _ => println!("No command specified"),
     }
 }
